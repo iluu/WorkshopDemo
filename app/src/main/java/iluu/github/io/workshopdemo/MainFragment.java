15e45a8ab9b39f5e836c0515564643adb5b4dff1
Karolina Rusin <kr@soundcloud.com>
2015-07-30 12:35:30 +0200

    [FitMe] Solution for step4

--- app/src/main/java/iluu/github/io/workshopdemo/MainFragment.java
+++ app/src/main/java/iluu/github/io/workshopdemo/MainFragment.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainFragment extends Fragment {
 
     public MainFragment() {
@@ -18,6 +17,23 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
+        final EditText brandInput = (EditText) view.findViewById(R.id.brand_input);
+        final EditText sizeInput = (EditText) view.findViewById(R.id.size_input);
+        final Button addButton = (Button) view.findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String brand = brandInput.getText().toString();
+                int size = Integer.parseInt(sizeInput.getText().toString());
+
+                Toast.makeText(getActivity(), getString(R.string.new_shoes_added, brand, size), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        return view;
     }
 }
